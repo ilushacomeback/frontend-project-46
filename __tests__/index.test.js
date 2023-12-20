97dda61d8c6dcb24bcde61369349065ecdbe9426
ilushacomeback <vip.ilya.rud.2016@mail.com>
2023-12-20 14:52:28 +0300

    test

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import genDiff from "../src/index.js";
+
+const res = `{
+\t- follow: false
+\t  host: hexlet.io
+\t- proxy: 123.234.53.22
+\t- timeout: 50
+\t+ timeout: 20
+\t+ verbose: true
+}`;
+
+test("work", () => {
+  expect(
+    genDiff("./__fixtures__/file1.json", "./__fixtures__/file2.json")
+  ).toEqual(res);
+});
--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import fs from 'fs';
-import process from 'node:process';
-import path from 'path';
-import _ from 'lodash';
-import parser from './parser.js';
+import fs from "fs";
+import process from "node:process";
+import path from "path";
+import _ from "lodash";
+import parser from "./parser.js";
 
-const getExtension = (file) => file.split('.').at(-1);
+const getExtension = (file) => file.split(".").at(-1);
 const getPath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
-const readFile = (pathToFile) => fs.readFileSync(getPath(pathToFile), 'utf-8');
+const readFile = (pathToFile) => fs.readFileSync(getPath(pathToFile), "utf-8");
 
 const genDiff = (filePath1, filePath2) => {
   const file1 = parser(readFile(filePath1), getExtension(filePath1));
   const file2 = parser(readFile(filePath2), getExtension(filePath2));
-  const keys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)))
-    .map((key) => {
+  const keys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2))).map(
+    (key) => {
       if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
         if (file1[key] === file2[key]) {
-          console.log(`    ${key}: ${file1[key]}`);
+          return `\t  ${key}: ${file1[key]}`;
         } else {
-          console.log(`  - ${key}: ${file1[key]}`);
-          console.log(`  + ${key}: ${file2[key]}`);
+          const one = `\t- ${key}: ${file1[key]}`
+          const two = `\t+ ${key}: ${file2[key]}`
+          return `${one}\n${two}`;
         }
       }
       if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-        console.log(`  - ${key}: ${file1[key]}`);
+        return `\t- ${key}: ${file1[key]}`;
       }
       if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-        console.log(`  + ${key}: ${file2[key]}`);
+        return `\t+ ${key}: ${file2[key]}`;
       }
-    });
-  return keys;
+    }
+  );
+  console.log(`{\n${keys.join("\n")}\n}`);
+  return `{\n${keys.join("\n")}\n}`;
 };
 export default genDiff;
