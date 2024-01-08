ed0ffc3bb3f09e32708dab4a3434b9d63fb01391
ilushacomeback <vip.ilya.rud.2016@mail.com>
2024-01-08 13:44:52 +0300

    test for new fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import genDiff from '../src/index.js';
+import genDiff from "../src/index.js";
 
 const res = `{
 \t- follow: false
@@ -10,11 +10,65 @@ const res = `{
 \t+ verbose: true
 }`;
 
-test('work', () => {
+const res2 = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}`;
+
+test("work", () => {
   expect(
-    genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json'),
+    genDiff("./__fixtures__/file1.json", "./__fixtures__/file2.json")
   ).toEqual(res);
   expect(
-    genDiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml'),
+    genDiff("./__fixtures__/file1.yaml", "./__fixtures__/file2.yaml")
   ).toEqual(res);
+});
+
+test("work2", () => {
+  expect(
+    genDiff("./__fixtures__/filepath1.json", "./__fixtures__/filepath2.json")
+  ).toEqual(res2);
+  expect(
+    genDiff("./__fixtures__/filepath1.yml", "./__fixtures__/filepath2.yml")
+  ).toEqual(res2);
 });
